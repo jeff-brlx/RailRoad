25b5fa736f96bec3df8a1d714b30a8be85d14116
jeff_brlx <jeffreyburluraux@gmail.com>
2024-10-24 18:19:43 +0200

    fix: soft delete

--- models/trains.models.js
+++ models/trains.models.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 
 const trainSchema = new mongoose.Schema({
-    end_station: { type: String, required: true },
-    time_of_departure: { type: Date, required: true },
-    status:{type: String, default:'active'}
     name: {type: String, required: true, unique: true},
     start_station: {type: String, required: true},
     end_station: {type: String, required: true},
