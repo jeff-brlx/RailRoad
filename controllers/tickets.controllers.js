5e9fa17cb234416f5f8b3c879d60937332bbd2b7
Idika-lionnel <146442317+Idika-lionnel@users.noreply.github.com>
2024-10-25 11:05:21 +0200

    Update tickets.controllers.js

--- controllers/tickets.controllers.js
+++ controllers/tickets.controllers.js
@@ -1,22 +1,31 @@
 // tickets.controllers.js
 const Ticket = require('../models/tickets.models');
 const Trainstation = require('../models/trainstations.models');
+const User = require('../models/userModel'); 
 
 const bookTicket = async (req, res) => {
     const { userId, trainstationId } = req.body;
 
     try {
+        // Vérifier si l'utilisateur existe
+        const user = await User.findById(userId);
+        if (!user || user.status === 'deleted') {
+            return res.status(404).send("User not found");
+        }
+
+        // Vérifier si la station de train existe et est active
         const trainstation = await Trainstation.findById(trainstationId);
         if (!trainstation || trainstation.status === 'deleted') {
             return res.status(404).send("Trainstation not found or has been deleted");
         }
 
-        // Check if the trainstation is open
-        const currentHour = new Date().toISOString().slice(11, 16); // Get HH:mm format
+        // Vérifier si la station est ouverte pour les réservations
+        const currentHour = new Date().toISOString().slice(11, 16); // Format HH:mm
         if (currentHour < trainstation.open_hour || currentHour > trainstation.close_hour) {
             return res.status(400).send("Trainstation is currently closed for bookings");
         }
 
+        // Créer et enregistrer un nouveau ticket
         const newTicket = new Ticket({
             userId,
             trainstationId
